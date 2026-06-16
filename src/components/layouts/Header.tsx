331faef4e71f342b388e8de1babba8e61898eb3b
urzk <shgerk@gmail.com>
2026-06-16 14:54:04 +0900

    adjusted style of header nav

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -6,16 +6,16 @@ const Header = () => {
       <div className="white-land h-16">
         <div className="mx-auto flex h-full max-w-[1048px] px-6 py-2">
           <ul className="flex">
-            <li className="header-nav-list-item-border px-2">
+            <li className="header-nav-list-item-border px-0.5">
               <Link href="/">
-                <div className="flex h-full from-white via-slate-100 to-white px-2 hover:bg-gradient-to-b [&>*]:my-auto">
+                <div className="flex h-full px-3 rounded-md hover:bg-slate-50 transition [&>*]:my-auto">
                   <span className="text-slate-800">HOME</span>
                 </div>
               </Link>
             </li>
-            <li className="header-nav-list-item-border px-4">
+            <li className="header-nav-list-item-border px-0.5">
               <Link href="/posts">
-                <div className="flex h-full [&>*]:my-auto">
+                <div className="flex h-full px-3 rounded-md hover:bg-slate-50 transition [&>*]:my-auto">
                   <span className="text-slate-800">POSTS</span>
                 </div>
               </Link>
