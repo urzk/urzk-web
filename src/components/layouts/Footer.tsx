a7de359d9a94c07e39f47be05d92184bbfb4f662
urzk <shgerk@gmail.com>
2026-06-17 11:09:20 +0900

    adjusted footer style

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="pt-3">
-      <div className="white-land h-16">
-        <div className="py-3 text-center">
+      <div className="white-land flex h-16 items-center justify-center">
+        <div className="text-center">
           <small>&copy; 2026 Urozuki.</small>
         </div>
       </div>
