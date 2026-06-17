3b6e4eed57bfc4282fcfe478957fbe4b18eb089c
urzk <shgerk@gmail.com>
2026-06-17 11:42:55 +0900

    updated header menu

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -7,6 +7,8 @@ const Header = () => {
         <div className="mx-auto flex h-full max-w-[1048px] px-6 py-2">
           <ul className="flex">
             <HeaderNavListItem href="/">HOME</HeaderNavListItem>
+            <HeaderNavListItem href="/posts/profile">PROFILE</HeaderNavListItem>
+            <HeaderNavListItem href="/posts/works">WORKS</HeaderNavListItem>
             <HeaderNavListItem href="/posts">POSTS</HeaderNavListItem>
           </ul>
         </div>
