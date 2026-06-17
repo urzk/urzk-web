4235198130f5a01d82a6d2a1c7fb5b9394cc8342
urzk <shgerk@gmail.com>
2026-06-17 11:51:53 +0900

    updated top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Nav from "~/components/layouts/Nav";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,7 +8,17 @@ export default function Home() {
       <main className="flex flex-col grow p-3">
         <div className="cell-wrapper">
           <div className="cell prose prose-slate max-w-none max-sm:prose-sm">
-            ここに本文が入るよ！
+            <p>
+              <strong>こちらは、urzk/虚月希音のWebサイトです。</strong>
+              <br />
+              <Link href="/posts/profile" className="hover:underline">
+                自己紹介
+              </Link>
+              <br />
+              <Link href="/posts/works" className="hover:underline">
+                制作物など
+              </Link>
+            </p>
           </div>
         </div>
       </main>
